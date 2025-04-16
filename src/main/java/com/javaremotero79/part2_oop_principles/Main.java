22702f1b7dd00819a3f26c9cb51a5fec4b8581be
Iancu Jianu <iancujianu98@gmail.com>
2025-04-16 20:08:51 +0300

    adaugat cod de test in main pt inheritance

--- src/main/java/com/javaremotero79/part2_oop_principles/Main.java
+++ src/main/java/com/javaremotero79/part2_oop_principles/Main.java
@@ -10,6 +10,20 @@ public class Main {
         userAccount.setUsername("jianu14");
         userAccount.setPassword("catelusCuParulCret14*");
 
-        System.out.println(userAccount.getUsername());
+        System.out.println(userAccount.getUsername() + "\n");
+
+        /* Inheritance */
+        Employee employee = new Employee("1234", "Iancu", "Jianu", 27);
+        Chef chef = new Chef("9876", "Ion", "Ionel", 23, "Ajutor de bucatar");
+        Waiter waiter = new Waiter("5678", "Maria", "Mariuca", 29, 56.2);
+
+        System.out.println("Input din parent class Employee: " + employee.generateText());
+        employee.sayHi();
+
+        System.out.println("Input din parent class Chef: " + chef.generateText());
+        chef.sayHi();
+
+        System.out.println("Input din parent class Waiter: " + waiter.generateText());
+        waiter.sayHi();
     }
 }
