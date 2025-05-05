a3e749f1138aad75060db2536fe1620bde398164
Iancu Jianu <iancujianu98@gmail.com>
2025-05-05 21:00:49 +0300

    - added treemap example

---
+++ src/main/java/com/javaremotero79/part3_dsa_collections/map/book/Book.java
@@ -0,0 +1,35 @@
+package com.javaremotero79.part3_dsa_collections.map.book;
+
+public class Book {
+
+    private String titlu;
+    private String autor;
+    int an;
+
+    public Book(String titlu, String autor, int an) {
+        this.titlu = titlu;
+        this.autor = autor;
+        this.an = an;
+    }
+
+    public String getTitlu() {
+        return titlu;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAn() {
+        return an;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "titlu='" + titlu + '\'' +
+                ", autor='" + autor + '\'' +
+                ", an=" + an +
+                '}';
+    }
+}
